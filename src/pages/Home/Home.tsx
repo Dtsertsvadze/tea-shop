b68f5244c4af5bb35fe629087c258b2d9c41d8da
ModernDesigners <120728081+AtukaJikuridze@users.noreply.github.com>
2023-09-15 02:48:46 +0400

    Added OptionsInfo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import "./Home.css";
 import FirstSection from "./components/FirstSection/FirstSection";
+import OptionsInfo from "./components/OptionsInfo/OptionsInfo";
 export default function Home() {
   return (
     <div>
       <FirstSection />
+      <OptionsInfo />
     </div>
   );
 }
